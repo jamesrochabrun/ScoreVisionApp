059b1fee466009f5c2a14da1206a9adfac371d77
James Rochabrun <j@Jamess-MacBook-Pro-2.local>
2018-02-01 08:34:17 -0800

    first commit

---
+++ ScoreVisionApp/Controller/PhotoListController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreVisionApp
+//
+//  Created by James Rochabrun on 1/31/18.
+//  Copyright © 2018 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+final class PhotoListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ ScoreVisionApp/Tools/PhotoPickerManager.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoPickerManager.swift
+//  ScoreVisionApp
+//
+//  Created by James Rochabrun on 1/31/18.
+//  Copyright © 2018 James Rochabrun. All rights reserved.
+//
+
+import Foundation
