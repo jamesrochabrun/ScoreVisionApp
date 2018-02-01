5306242ad5b98d6e9627de90f032f257604181d4
James Rochabrun <j@Jamess-MacBook-Pro-2.local>
2018-02-01 08:34:56 -0800

    first commit

--- ScoreVisionApp/Controller/PhotoListController.swift
+++ ScoreVisionApp/Controller/PhotoListController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 final class PhotoListController: UIViewController {
 
+    lazy var photoPickerManager: PhotoPickerManager = {
+        let manager = PhotoPickerManager(presentingController: self)
+        manager.delegate = self
+        return manager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
+    
+    @IBAction func launchLibrary(_ sender: UIButton) {
+        photoPickerManager.presentPhotoPicker(from: CameraSourceType.librarySource, animated: true)
+    }
+    
+    @IBAction func launchCamera(_ sender: UIButton) {
+        photoPickerManager.presentPhotoPicker(from: CameraSourceType.cameraSource, animated: true)
+    }
 }
 
+
+
+extension PhotoListController: PhotoPickerManagerDelegate {
+    
+    func manager(_ manager: PhotoPickerManager, didPickImage image: UIImage) {
+
+        // here is our image
+        manager.dismissPhotoPicker(animated: true) {
+            print("dismiss")
+        }
+    }
+}
--- ScoreVisionApp/Tools/PhotoPickerManager.swift
+++ ScoreVisionApp/Tools/PhotoPickerManager.swift
@@ -5,5 +5,60 @@
 //  Created by James Rochabrun on 1/31/18.
 //  Copyright © 2018 James Rochabrun. All rights reserved.
 //
+import UIKit
+import MobileCoreServices
 
-import Foundation
+enum CameraSourceType {
+    case cameraSource
+    case librarySource
+}
+
+protocol PhotoPickerManagerDelegate: class {
+    func manager(_ manager: PhotoPickerManager, didPickImage image: UIImage)
+}
+class PhotoPickerManager: NSObject {
+    
+    private let imagePickerController = UIImagePickerController()
+    private let presentingController: UIViewController
+    weak var delegate: PhotoPickerManagerDelegate?
+    
+    init(presentingController: UIViewController) {
+        self.presentingController = presentingController
+        super.init()
+        configure()
+    }
+    
+    /// dismissing picker
+    func dismissPhotoPicker(animated: Bool, completion: (() -> Void)?) {
+        imagePickerController.dismiss(animated: animated, completion: completion)
+    }
+    
+    /// configure picker
+    private func configure() {
+
+        /// only camera photo no videos
+        imagePickerController.mediaTypes = [kUTTypeImage as String]
+        imagePickerController.delegate = self
+    }
+    
+    /// presenting picker from source type
+    func presentPhotoPicker(from source: CameraSourceType, animated: Bool) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) && source == .cameraSource  {
+            imagePickerController.sourceType = .camera
+            imagePickerController.cameraDevice = .front
+        } else {
+            imagePickerController.sourceType = .photoLibrary
+        }
+        presentingController.present(imagePickerController, animated: animated, completion: nil)
+    }
+}
+extension PhotoPickerManager: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        /// getting original image
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        self.delegate?.manager(self, didPickImage: image)
+    }
+}
